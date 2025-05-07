8cca1f1b7b5b38dc77a6ebaad47ad12a3b802563
Junpeng-Wang-TUM <47067711+Junpeng-Wang-TUM@users.noreply.github.com>
2025-05-07 21:37:43 +0200

    update 1

--- TOP3D_XL.m
+++ TOP3D_XL.m
@@ -6,8 +6,8 @@ function TOP3D_XL(inputModel, consType, V0, nLoop, rMin, varargin)
 	%%input volume: true(nely,nelx,nelz)
 	%%TOP3D_XL(true(50,100,50), 'GLOBAL', 0.12, 50, sqrt(3)); $$ Run Topology Optimization on the Built-in Cuboid Design Domain
 	%%TOP3D_XL(true(50,100,50), 'LOCAL', 0.5, 300, sqrt(3), 6); $$ Run Porous Infill Optimization on the Built-in Cuboid Design Domain
-	%%TOP3D_XL('./data/femur.TopVoxel', 'GLOBAL', 0.4, 50, sqrt(3)); $$ Run Topology Optimization on the External Design Domain Provided in *.TopVoxel
-	%%TOP3D_XL('./data/femur.TopVoxel', 'LOCAL', 0.5, 300, sqrt(3), 6); $$ Run Porous Infill Optimization on the External Design Domain Provided in *.TopVoxel
+	%%TOP3D_XL('./data/Femur.TopVoxel', 'GLOBAL', 0.4, 50, sqrt(3)); $$ Run Topology Optimization on the External Design Domain Provided in *.TopVoxel
+	%%TOP3D_XL('./data/Femur.TopVoxel', 'LOCAL', 0.5, 300, sqrt(3), 6); $$ Run Porous Infill Optimization on the External Design Domain Provided in *.TopVoxel
 	%% Download External Datasets (Femur, Molar, GEbracket): https://syncandshare.lrz.de/getlink/fiW6M69m5HoTUcH4T7wLKZ/
 	switch consType
 		case 'GLOBAL'
